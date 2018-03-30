ff84d58c00f91a9a73ec733d223bab8662267131
blava <misha@mblavatsky.zeg>
2018-03-30 19:54:03 +0400

    problem 17 bugged

--- problem_17.py
+++ problem_17.py
@@ -1,7 +1,7 @@
 letters = {
             "1" : "one",
             "2" : "two",
-            "3" : "tree",
+            "3" : "three",
             "4" : "four",
             "5" : "five",
             "6" : "six",
@@ -9,7 +9,7 @@ letters = {
             "8" : "eight",
             "9" : "nine",
             "10" : "ten",
-            "11" : "evelen",
+            "11" : "eleven",
             "12" : "twelve",
             "13" : "thirteen",
             "14" : "fourteen",
@@ -20,32 +20,65 @@ letters = {
             "19" : "nineteen",
             "20" : "twenty",
             "30" : "thirty",
-            "40" : "fourty",
+            "40" : "forty",
             "50" : "fifty",
             "60" : "sixty",
             "70" : "seventy",
             "80" : "eighty",
             "90" : "ninety",
-            "100" : "houdred",
+            "100" : "hundred",
             "1000" : "thousand" }   
             
-def spellTheNumber (number):
-   spell = []
-   divider = 1000
-   if (number // divider) == 1:
-      spell.append(letters["1"])
-      spell.append(letters["1000"])
-      number %=1000;
-   if number // 100 > 0:
-         spell.append(letters[str(number // 100)])
-         spell.append(letters["100"])
-   number %=100                     
-   if (number >20 ):
-         spell.append(letters[str((number // 10) * 10)])
-         spell.append(letters[str(number % 10)])
-   else:
-         spell.append(letters[str(number)])         
-   print(spell)
-   
-   
-spellTheNumber(200)
+def classify(n):
+    spell = []
+    divider = 1000
+    while(divider >= 1):
+        n1 = n // divider
+        n2 = n % divider
+        spell.append(n1)
+        n = n2
+        divider = divider // 10
+    print(spell)        
+    return spell
+
+def spellIt(l):
+    sp = []
+    ln = len(l)
+
+    if (l[0] != 0):
+        sp.append(letters[str(l[0])])
+        sp.append(letters["1000"])
+    
+    if ( l[1] != 0):
+        sp.append(letters[str(l[1])])
+        sp.append(letters["100"])
+        sp.append("and")
+
+    less20 = False    
+    if ( l[2] != 0):
+        if (l[2] >= 2):
+            sp.append(letters[str(l[2] * 10)])
+        else:
+            less20 = True
+            
+    if (l[3] != 0):
+        if less20:
+            dex = 10 + l[3];
+            print(dex)
+            sp.append(letters[str(10 + l[3])])
+        else:            
+            sp.append(letters[str(l[3])])
+
+    print(sp)
+    sz = 0
+    for i in sp:
+        sz += len(i)
+    print(sz)    
+    return sz
+
+s = 0    
+#for index in range(1, 101):
+lst = classify(110)
+s += spellIt(lst)
+
+print(s)    
